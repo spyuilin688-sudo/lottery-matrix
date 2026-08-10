4cf1b28041fff86f340361e4bef9610b55773bc4
spyuilin688-sudo <spyuilin688@gmail.com>
2026-08-10 14:52:23 +0800

    啟用統一版面規則

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "@fontsource/roboto/latin-500.css";
 import App from "./App";
 import "./styles.css";
 import "./prototype.css";
+import "./layout-canonical.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
