da7c2cb4b66b69e05e19fbe1aec8dab32a96cf96
spyuilin688-sudo <spyuilin688@gmail.com>
2026-08-11 02:06:55 +0800

    feat: load shared lottery ball styling

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import "@fontsource/roboto/latin-500.css";
 import App from "./App";
 import "./styles.css";
 import "./prototype.css";
+import "./number-ball.css";
+import "./number-ball-bridge.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
