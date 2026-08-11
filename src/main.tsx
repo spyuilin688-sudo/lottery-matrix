585b94d7cc7df6348af18e471cf28ce0e73bc39d
spyuilin688-sudo <spyuilin688@gmail.com>
2026-08-11 13:59:32 +0800

    Import homepage repair styles on current main

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import "./styles.css";
 import "./prototype.css";
 import "./brand-header-unify.css";
+import "./homepage-repair.css";
 import "./tongxing-compact.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
