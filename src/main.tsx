316eb3bd476eeceffc61142a7e15aaefb479617b
spyuilin688-sudo <spyuilin688@gmail.com>
2026-08-11 13:22:07 +0800

    Load Matrix TongXing compact styles

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from "./App";
 import "./styles.css";
 import "./prototype.css";
 import "./brand-header-unify.css";
+import "./tongxing-compact.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
