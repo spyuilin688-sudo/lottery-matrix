154874572f88e22e49f8f833d74829b4aff84c04
spyuilin688-sudo <spyuilin688@gmail.com>
2026-08-11 10:41:25 +0800

    style: load unified brand header rules

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "@fontsource/roboto/latin-500.css";
 import App from "./App";
 import "./styles.css";
 import "./prototype.css";
+import "./brand-header-unify.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
