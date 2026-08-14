0d589b9c4396649b70be8afed8f31e06d50192c1
spyuilin688-sudo <spyuilin688@gmail.com>
2026-08-14 17:12:34 +0800

    修正近10期六合彩數字與底線對齊

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "@fontsource/roboto/latin-500.css";
+import "@fontsource/roboto/latin-700.css";
 import App from "./App";
 import "./styles.css";
 import "./prototype.css";
