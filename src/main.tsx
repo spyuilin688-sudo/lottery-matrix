8c06539e8b842010c77d78a1591cd86cfc2f0025
spyuilin688-sudo <spyuilin688@gmail.com>
2026-08-16 05:45:33 +0800

    Apply centralized input behavior across the app

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "@fontsource/roboto/latin-500.css";
 import "@fontsource/roboto/latin-700.css";
 import App from "./App";
+import { installGlobalInputBehavior } from "./input-behavior";
 import "./styles.css";
 import "./prototype.css";
 import "./brand-header-unify.css";
 import "./homepage-repair.css";
 import "./tongxing-compact.css";
 import "./admin/admin.css";
+
+installGlobalInputBehavior();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
