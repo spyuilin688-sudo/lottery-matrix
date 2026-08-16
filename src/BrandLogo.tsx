83610cd2cf4de00f72d3e1ed90feaec6a9bc4bac
spy854540-cmd <spy854540@gmail.com>
2026-08-16 08:18:05 +0800

    Unify brand logo source across all pages

--- src/BrandLogo.tsx
+++ src/BrandLogo.tsx
@@ -1,4 +1,4 @@
-export const PRIMARY_BRAND_LOGO = "/assets/lottery/functions/NewLogo.png";
+export const PRIMARY_BRAND_LOGO = "/assets/lottery/functions/NewLogo.png?v=20260816-2";
 
 export type BrandLogoProps = {
   className?: string;
