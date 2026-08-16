bf434a935dc83167dbcab4dbc6888ac5a87ed86e
spy854540-cmd <spy854540@gmail.com>
2026-08-16 08:14:26 +0800

    Force refresh latest bottom navigation artwork

--- src/BottomNavigation.tsx
+++ src/BottomNavigation.tsx
@@ -59,7 +59,7 @@ export function BottomNavigation({
     >
       <img
         className="bottom-navigation-artwork"
-        src={`/assets/lottery/functions/${activeArtwork}`}
+        src={`/assets/lottery/functions/${activeArtwork}?v=20260816-2`}
         alt="Matrix 底部導覽"
         draggable={false}
       />
