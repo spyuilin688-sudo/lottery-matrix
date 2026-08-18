a1b23c365416b14e408c4231045689f16e6e551d
spyuilin688-sudo <spyuilin688@gmail.com>
2026-08-19 03:45:21 +0800

    Wire Matrix Explore layout debug mode

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "@fontsource/roboto/latin-500.css";
 import "@fontsource/roboto/latin-700.css";
 import App from "./App";
 import { installGlobalInputBehavior } from "./input-behavior";
+import { installLayoutDebugMode } from "./layout-debug";
 import "./styles.css";
 import "./prototype.css";
 import "./brand-header-unify.css";
@@ -13,6 +14,7 @@ import "./admin/admin.css";
 import "./matrix-explore-spacing.css";
 
 installGlobalInputBehavior();
+installLayoutDebugMode();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
