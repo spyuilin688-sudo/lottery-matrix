38c61e81acbe78409655eedd13a64920010ba287
spyuilin688-sudo <spyuilin688@gmail.com>
2026-08-19 03:50:04 +0800

    Restore protected mobile runtime entry

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import "@fontsource/roboto/latin-500.css";
 import "@fontsource/roboto/latin-700.css";
 import App from "./App";
 import { installGlobalInputBehavior } from "./input-behavior";
-import { installLayoutDebugMode } from "./layout-debug";
 import "./styles.css";
 import "./prototype.css";
 import "./brand-header-unify.css";
@@ -14,7 +13,6 @@ import "./admin/admin.css";
 import "./matrix-explore-spacing.css";
 
 installGlobalInputBehavior();
-installLayoutDebugMode();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
