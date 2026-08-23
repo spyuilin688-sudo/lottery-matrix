92ad1f4529c7409094ff89674316eb98cc4e7fca
spyuilin688-sudo <spyuilin688@gmail.com>
2026-08-23 21:48:48 +0800

    Restore notification Bottom Sheet flow

--- src/FeaturePagesPatched.tsx
+++ src/FeaturePagesPatched.tsx
@@ -3,7 +3,7 @@ import {
   QuickNavigationProvider,
   type ScreenId,
 } from "./FeaturePagesCore";
-import { NotificationsPagePatched } from "./NotificationsPagePatched";
+import "./feature-page-adjustments.css";
 
 export { QuickNavigationProvider };
 export type { ScreenId };
@@ -27,17 +27,6 @@ export function FeaturePageRouter({
   onNavigate: Navigate;
   historyReturnScreen?: ScreenId;
 } & BottomNavCallbacks) {
-  if (screen === "notifications") {
-    return (
-      <NotificationsPagePatched
-        onNavigate={onNavigate}
-        onQuickOpen={onQuickOpen}
-        onQuickConfigure={onQuickConfigure}
-        quickActive={quickActive}
-      />
-    );
-  }
-
   return (
     <CoreFeaturePageRouter
       screen={screen}
