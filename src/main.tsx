aad5fb738ac75ef6d1244d2cdd04f34a8b4c1b77
spyuilin688-sudo <spyuilin688@gmail.com>
2026-08-25 01:03:45 +0800

    Load Pro plan carousel swipe affordance styles

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,7 @@ import "./responsive-feature-pages.css";
 import "./tongxing-compact.css";
 import "./admin/admin.css";
 import "./matrix-explore-spacing.css";
+import "./pro-plans-carousel-peek.css";
 import { matrixApiFetch } from "./matrix-api-client";
 import { startMemberOnlineTracking } from "./member-online";
 
