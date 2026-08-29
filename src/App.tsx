7d7355b23ae04583b0da544feeaefeae03f6bfd6
spyuilin688-sudo <spyuilin688@gmail.com>
2026-08-30 02:29:12 +0800

    feat: route isolated explore result preview page

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,30 @@ import { MobileDeviceProvider } from "./mobile/Device";
 import { KeyboardProvider } from "./mobile/Keyboard";
 import Prototype from "./Prototype";
 import { AppDialogProvider } from "./dialog/AppDialog";
+import { ExploreResultPreviewPage } from "./ExploreResultPreviewPage";
 
 export default function App() {
   const isAdminPath =
     window.location.pathname === "/admin" ||
     window.location.pathname.startsWith("/admin/");
+  const isExploreResultPreviewPath =
+    window.location.pathname === "/explore-result-preview" ||
+    window.location.pathname === "/explore-result-preview/";
 
   return (
     <AppDialogProvider>
       {isAdminPath ? <AdminApp /> : (
         <div className="app-mobile-canvas" data-testid="app-mobile-canvas">
-          <MemberSessionBridge />
-          <MobileDeviceProvider>
-            <KeyboardProvider>
-              <Prototype />
-            </KeyboardProvider>
-          </MobileDeviceProvider>
+          {isExploreResultPreviewPath ? <ExploreResultPreviewPage /> : (
+            <>
+              <MemberSessionBridge />
+              <MobileDeviceProvider>
+                <KeyboardProvider>
+                  <Prototype />
+                </KeyboardProvider>
+              </MobileDeviceProvider>
+            </>
+          )}
         </div>
       )}
     </AppDialogProvider>
