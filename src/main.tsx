ff1fffec7ff3b524d332f89b463066e3e0cada5f
spyuilin688-sudo <spyuilin688@gmail.com>
2026-08-31 03:14:08 +0800

    style: load notification visual refinements

--- src/main.tsx
+++ src/main.tsx
@@ -13,6 +13,7 @@ import "./responsive-feature-pages.css";
 import "./tongxing-compact.css";
 import "./matrix-explore-spacing.css";
 import "./feature-page-adjustments.css";
+import "./notification-visual-refinement.css";
 import "./number-reference-visual-refinement.css";
 
 import { startMemberOnlineTracking } from "./member-online";
