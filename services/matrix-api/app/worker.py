96e84923ea79b8208ee613afa98443687fa0f232
spyuilin688-sudo <spyuilin688@gmail.com>
2026-08-31 12:34:32 +0800

    feat: isolate shared Matrix pipeline as v8

--- services/matrix-api/app/worker.py
+++ services/matrix-api/app/worker.py
@@ -26,7 +26,7 @@ TIANGONG_BATCH_SIZE = 3
 MAX_CYCLES_PER_INVOCATION = 450
 MAX_FAILURES_PER_INVOCATION = 3
 RETRY_BACKOFF_SECONDS = (15.0, 45.0)
-ANALYSIS_VERSION = "matrix-python-v7"
+ANALYSIS_VERSION = "matrix-python-v8"
 
 
 def _is_transient_service_error(error: Exception) -> bool:
