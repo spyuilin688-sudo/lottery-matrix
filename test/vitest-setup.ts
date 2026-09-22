debd772fcc9a9b4a47cef2e44c1ba9ce097e052c
spyuilin688-sudo <spyuilin688@gmail.com>
2026-09-22 18:10:02 +0800

    test(auth): reset shared session state between tests

--- test/vitest-setup.ts
+++ test/vitest-setup.ts
@@ -1,8 +1,9 @@
 import "./dialog-test-setup";
 import { cleanup } from "@testing-library/react";
 import { afterEach } from "vitest";
+import { resetMemberSessionStoreForTests } from "../src/auth/member-session-store";
 
-afterEach(() => cleanup());
+afterEach(() => { cleanup(); resetMemberSessionStoreForTests(); });
 
 if (typeof HTMLElement !== "undefined" && typeof HTMLElement.prototype.scrollTo !== "function") {
   HTMLElement.prototype.scrollTo = function scrollTo() {};
